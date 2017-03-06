218a793b006ca053309930840601b8d93f409806
Talgat <talgat_ruby@yahoo.com>
2017-03-06 14:45:27 +0600

    Finished project

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,74 @@
 import React, { Component } from 'react';
+import $class from 'classnames';
 import './App.css';
 
 import Timeline from './timeline/Timeline';
 
+const TYPES = {
+    HIGH: 'HIGH',
+    LOW: 'LOW'
+}
+
 class App extends Component {
-    state = {
-        values: [1, 2, 3, 4, 5]
+    initState = {
+        values: [],
+        guessNumber: '',
+        value: '',
+        correct: false,
+        right: true
+    }
+
+    state = this.initState;
+
+    componentWillMount = () => {
+        const guessNumber = Math.floor(Math.random() * (999 - 1)) + 1;
+        this.setState({ guessNumber });
+    }
+
+    onChange = ({ target: { value } }) => {
+        this.setState({ value: Number(value) });
+    }
+
+    submit = () => {
+        const { guessNumber, value, values: prevValues, right: prevRight } = this.state;
+        if(value === guessNumber) {
+            this.setState({ correct: true });
+        } else {
+            let values = [...prevValues, {
+                value,
+                type: value > guessNumber ? TYPES.HIGH : TYPES.LOW
+            }];
+            let right = prevRight;
+
+            if(values.length > 5) {
+                values = [...values.slice(1)];
+                right = !prevRight;
+            }
+
+            this.setState({ values, right });
+        }
+    }
+
+    reset = () => {
+        this.setState(this.initState);
     }
 
     render = () => (
         <div className="app">
             <header className="App-header">
-                <Timeline values={this.state.values}/>
+                <Timeline values={this.state.values} startRight={this.state.right}/>
             </header>
-            <p className="App-intro">
-                To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
+            <main>
+                <input type="text" value={this.state.value} onChange={this.onChange}/>
+                <button onClick={this.submit}>Submit</button>
+            </main>
+            <section className={$class('modal-window', { open: this.state.correct })}>
+                <div>
+                    <h2>Correct!!!</h2>
+                    <p>Congrats, you found the number {this.state.value}</p>
+                    <button onClick={this.reset}>RESET</button>
+                </div>
+            </section>
         </div>
     )
 }
--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -2,16 +2,19 @@ import React from 'react';
 import $class from 'classnames';
 import './Timeline.css';
 
-const Timeline = ({ values }) => {
-	const renderValues = (item, index) => (
-		<div className={$class({ right: index % 2 === 0, left: index % 2 !== 0 })}>
-			<div className="flag-wrapper">
-				<span className="flag">freelancer</span>
-				<span className="time-wrapper">2013-present</span>
-			</div>
-			<div className="desc">My current employment. Way better than the position before!</div>
-		</div>
-	);
+const Timeline = ({ values, startRight  }) => {
+	const renderValues = ({ value, type }, index) => {
+		const rightSide = startRight === (index % 2 === 0);
+
+		return (
+			<li className={$class({ right: rightSide, left: !rightSide })} key={index}>
+				<div className="flag-wrapper">
+					<span className="flag">{type}</span>
+				</div>
+				<div className="desc">{value}</div>
+			</li>
+		);
+	};
 
 	return (
 		<ul className="timeline">
